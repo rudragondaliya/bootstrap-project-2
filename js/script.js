7068a381f33b2741340a39b116a00239db595ae1
rudragondaliya <gondaliyarudra61@gmail.com>
2024-08-12 23:16:38 +0530

    done

--- js/script.js
+++ js/script.js
@@ -5,14 +5,14 @@ $(function() {
 })
 
 
-document.addEventListener("DOMContentLoaded", function(){
-    const navigation = document.querySelector('.navbar');
+// document.addEventListener("DOMContentLoaded", function(){
+//     const navigation = document.querySelector('.navbar');
 
-    window.addEventListener('scroll', () => {
-        if(window.scrollY > 0){
-            navigation.classList.add('scrolled');
-        }else{
-            navigation.classList.remove('scrolled');
-        }
-    });
-});
+//     window.addEventListener('scroll', () => {
+//         if(window.scrollY > 0){
+//             navigation.classList.add('scrolled');
+//         }else{
+//             navigation.classList.remove('scrolled');
+//         }
+//     });
+// });
