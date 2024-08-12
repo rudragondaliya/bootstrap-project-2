0c75246453a70b75118f350c1408277e1713384c
rudragondaliya <gondaliyarudra61@gmail.com>
2024-08-12 11:09:26 +0530

    done

--- js/script.js
+++ js/script.js
@@ -3,3 +3,16 @@ $(function() {
         animationType: 'clip'
     });
 })
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    const navigation = document.querySelector('.navbar');
+
+    window.addEventListener('scroll', () => {
+        if(window.scrollY > 0){
+            navigation.classList.add('scrolled');
+        }else{
+            navigation.classList.remove('scrolled');
+        }
+    });
+});
